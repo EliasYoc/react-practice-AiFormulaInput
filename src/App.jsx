8e6aee51d48d9f259755425e4620d6f31a32ae27
elias isai yoc garcia <eliyoc_97@hotmail.com>
2026-04-02 21:23:04 -0600

    add: julieta mock

--- src/App.jsx
+++ src/App.jsx
@@ -119,8 +119,107 @@ const getFunctionTokens = (functionName) => {
   return functionTokenDict[functionName] || [];
 };
 
+const valueMock = [
+  {
+    order: 1,
+    type: "operator",
+    value: "IF(",
+    color: "#B2DFDB",
+  },
+  {
+    order: 2,
+    type: "operator",
+    value: "(",
+    color: "#E0E0E0",
+  },
+  {
+    order: 3,
+    type: "var",
+    value: "ingresos_operativos_mensuales_acumulados",
+    color: "#BBDEFB",
+  },
+  {
+    order: 4,
+    type: "operator",
+    value: "+",
+    color: "#B2DFDB",
+  },
+  {
+    order: 5,
+    type: "var",
+    value: "ingresos_extraordinarios_reportados_trimestralmente",
+    color: "#BBDEFB",
+  },
+  {
+    order: 6,
+    type: "operator",
+    value: ")",
+    color: "#E0E0E0",
+  },
+  {
+    order: 7,
+    type: "operator",
+    value: ">",
+    color: "#B2DFDB",
+  },
+  {
+    order: 8,
+    type: "var",
+    value: "limite_maximo_permitido_para_desviaciones_atipicas_historicas",
+    color: "#BBDEFB",
+  },
+  {
+    order: 9,
+    type: "operator",
+    value: ")",
+    color: "#E0E0E0",
+  },
+  {
+    order: 10,
+    type: "operator",
+    value: "ELSE(",
+    color: "#B2DFDB",
+  },
+  {
+    order: 11,
+    type: "operator",
+    value: "ROUND(",
+    color: "#B2DFDB",
+  },
+  {
+    order: 12,
+    type: "var",
+    value: "ingresos_operativos_mensuales_acumulados",
+    color: "#BBDEFB",
+  },
+  {
+    order: 13,
+    type: "operator",
+    value: ")",
+    color: "#E0E0E0",
+  },
+  {
+    order: 14,
+    type: "operator",
+    value: "==",
+    color: "#B2DFDB",
+  },
+  {
+    order: 15,
+    type: "var",
+    value: "objetivo_financiero_base_aprobado_comite",
+    color: "#BBDEFB",
+  },
+  {
+    order: 16,
+    type: "operator",
+    value: ")",
+    color: "#E0E0E0",
+  },
+];
+
 function App() {
-  const [value, setValue] = useState([]);
+  const [value, setValue] = useState(valueMock);
 
   return (
     <>
